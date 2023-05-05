172f61484c02042858ce637400793ecb5a017b0f
konst77 <rayyoung.moon@gmail.com>
2023-05-04 21:33:47 -0700

    modified:   src/pages/index.js 	src/components/ 	src/pages/about.js src/pages/learning.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,8 +4,15 @@ export default function Home() {
   return (
     <main>
       <Navbar />
-      <div >
-        Hero
+      <div className='grid gap-x-7 grid-cols-12'>
+        <div className='col-start-1 col-end-7'>
+          <h1 className='text-5xl'>
+            Ray crafts smooth experiences for digital products.
+            </h1>
+          <p className='text-lg text-slate-500 mt-3'>
+            With over 1 year of experience in understanding customer needs, turning that into product ideations and business strategies, telling brand stories to reach the next level.
+            </p>
+        </div>
       </div>
     </main>
   )
